1809d29608dcd97213d8c372aa37777bceb48387
kcapka <147353671+kcapka@users.noreply.github.com>
2024-01-25 18:30:25 -0800

    Added ability to purchase weapons

--- src/game.js
+++ src/game.js
@@ -4,48 +4,104 @@ import enemy1 from "./imgs/skeleton-enemy.webp";
 import "./game.css";
 import Popup from "./defeated";
 import Shop from './shop';
+import sword from "./imgs/sword-cursor.png";
+import axe from "./imgs/axe-cursor.png";
+
+/* 
+Todo:
+- Create enemy array of enemy objects. Include damage amount and amount of coin to receive upon defeat.
+    -Find out how to implement damage and reward as variables into functions
+    -Figure out how to Cycle through enemies on defeat
+- Create weapon choices. Creat weapon array of weapon objects that deal different damage and may be purchased.
+*/
+
+// WORK ON CLICKING SELECTED WEAPON AND DEALING CORRECT DAMAGE
+
+
+const weapons = [
+    {
+        id: Math.random(),
+        name: "Sword",
+        src: sword,
+        damage: 62,
+        price: 100
+},
+    {
+    id: Math.random(),
+    name: "Axe",
+    src: axe,
+    damage: 115,
+    price: 1000
+}
+];
 
 
 export default function Game() {
 
+    // Coin count, enemy health, and "defeated popup trigger"
     const [coinCount, setCoinCount] = useState(0);
     const [enemyHealth, setEnemyHealth] = useState(500);
     const [trigger, setTrigger] = useState(false);
+    const [myWeapons, setMyWeapons] = useState([weapons[0]]);
+    const [currentWeapon, setCurrentWeapon] = useState(myWeapons[0].damage);
 
+    //enemy object
     let skeletor = {src: enemy1, health: enemyHealth};
     
+    //handling damage put on enemy. Add damage amt to weapon?
     function handleDamage() {
-        setEnemyHealth(prevEnemyHealth => prevEnemyHealth - 62);
+        setEnemyHealth(prevEnemyHealth => prevEnemyHealth - currentWeapon);
     }
 
-    function handleResetEnemy() {
-        setTrigger(false);
-    }
+    const healthSection = !trigger ? `Health: ${skeletor.health}/500` : '';
 
-    function handlePurchase(price) {
-        setCoinCount(price);
-    }
-
-    const showReset = trigger ? "Reset Enemy" : '';
-    
-
-    const healthSection = 
-        !trigger ? `Health: ${skeletor.health}/500` : '';
-
-    
-
-
+    // adding coin for defeating enemy and triggering popup
     if (enemyHealth <= 0) {
         setCoinCount(prevCoinCount => prevCoinCount + 112);
         setEnemyHealth(500);
         setTrigger(true);
     }
+
+    //resetting enemy
+    function handleResetEnemy() {
+        setTrigger(false);
+    }
+    const showReset = trigger ? "Reset Enemy" : '';
+
+    // purchase prop to receive feedback from Shop component
+    function handlePurchase(price) {
+        setCoinCount(price);
+    }
+
+    function handleWeaponSelection(damage) {
+        setCurrentWeapon(damage);
+        console.log(currentWeapon);
+    }
+
+    function handleWeaponPurchase(item) {
+        setMyWeapons(item);
+    }
+
+    
+    
     
 
     return (
         <>
             <NavBar coin={coinCount} />
             <h1>Welcome to <span>The Fell Beast</span>! Use your weapon to defeat the enemy and gain coins.</h1>
+            <div className="weapon-inventory">
+                <h4>My Weapons</h4>
+                <div>
+                    {myWeapons.map((item) => (
+                        <div className="item" key={item.id} onClick={() => handleWeaponSelection(item.damage)}>
+                            <img src={item.src} />
+                            <p>{item.name}</p>
+                        </div>
+                ) )}
+                </div>
+            </div>
+            <div className="main-cont">
             <div className="enemies">
                 <p>Skeletal Mouthbreather</p>
                 <img src={skeletor.src} onClick={handleDamage} />
@@ -55,8 +111,10 @@ export default function Game() {
             <div className="reset-container">
                 <button onClick={handleResetEnemy} className="reset">{showReset}</button>
             </div>
-            <Shop purchase= {handlePurchase} coin={coinCount}/>
+            <Shop purchase= {handlePurchase} weaponPurchase ={handleWeaponPurchase} coin={coinCount}/>
+            </div>
         
         </>
     )
 }
+
--- src/shop.js
+++ src/shop.js
@@ -4,7 +4,32 @@ import map from "./imgs/map.png";
 import key from "./imgs/key-icon.png";
 import tools from "./imgs/tools-icon.png";
 import book from "./imgs/book-icon.png";
+import axe from "./imgs/axe-cursor.png";
+import sword from "./imgs/sword-cursor.png";
 
+const weapons = [
+    {
+        id: Math.random(),
+        name: "Sword",
+        src: sword,
+        damage: 62,
+        price: 100
+},
+    {
+    id: Math.random(),
+    name: "Axe",
+    src: axe,
+    damage: 115,
+    price: 1000
+},
+{
+    id: Math.random(),
+    name: "War Hammer",
+    src: sword,
+    damage: 500,
+    price: 3000
+}
+];
 
 const shopItems = [
     {
@@ -31,17 +56,16 @@ const shopItems = [
         name: "Book of Undead",
         price: 500
     }
-]
+];
+
+
 
 export default function Shop(props) {
 
+    // State array used to display items that have been purchased
     const [purchasedItems, setPurchasedItems] = useState([]);
 
-    function handleAddPurchasedItem(item) {
-        setPurchasedItems(prevPurchasedItems => [...prevPurchasedItems, item]);
-        
-    }
-
+    // Checks if you have enough coin to purchase and then takes money and triggers function to add purchased item to array
     function buyItem(item) {
         if (props.coin >= item.price) {
              props.purchase(prevCoinCount => prevCoinCount - item.price);
@@ -51,10 +75,34 @@ export default function Shop(props) {
          }
     }
 
+    function buyWeapon(item) {
+        if (props.coin >= item.price) {
+             props.purchase(prevCoinCount => prevCoinCount - item.price);
+             handlePurchasedWeapon(item);
+
+         } else {
+            alert("You do not have enough coins to purchase this item.");
+         }
+    }
+
+    // Function that adds purchased items to array
+    function handleAddPurchasedItem(item) {
+        setPurchasedItems(prevPurchasedItems => [...prevPurchasedItems, item]);
+        
+    }
+
+    function handlePurchasedWeapon(item) {
+        props.weaponPurchase(prevMyWeapons => [...prevMyWeapons, item]);
+        setPurchasedItems(prevPurchasedItems => [...prevPurchasedItems, item]);
+    }
+
+
+    //pulls purchased items menu in from out of frame
     function handleShowPurchased() {
         document.getElementById("purchased-items").classList.toggle("active");
         document.getElementById("purchased-title").classList.toggle("active");
     }
+
 
 
     return (
@@ -62,8 +110,15 @@ export default function Shop(props) {
             <h1>Shop</h1>
             <h2>Use your coins to purchase items below.</h2>
             <div className="item-container">
+            {weapons.map((item, index) => (
+                    <div className="item" key={index} onClick={() => buyWeapon(item)}>
+                        <img src={item.src} />
+                        <p>{item.name}</p>
+                        <p>{item.price} <span>coins</span></p>
+                    </div>
+            ) )}
             {shopItems.map((item) => (
-                    <div className="item" key={item.id} onClick={() => buyItem(item)}>
+                    <div className="item" key={item.name} onClick={() => buyItem(item)}>
                         <img src={item.src} />
                         <p>{item.name}</p>
                         <p>{item.price} <span>coins</span></p>
